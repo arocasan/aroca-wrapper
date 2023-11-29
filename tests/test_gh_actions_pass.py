09f347cd3b6452287505f12bf6f9252aa14136e5
arocasan <mr@michelantonio.com>
2023-11-30 00:36:42 +0100

    Fixed pytest and modified logger

--- tests/test_gh_actions_pass.py
+++
@@ -1,8 +0,0 @@
-class GitHubActionPassingTest:
-    def test_github_action_passing(self):
-        assert True
-
-
-class GitHubActionFailingTest:
-    def test_github_action_failing(self):
-        assert False
