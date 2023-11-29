4a0d5b3ea20c958e249a77751fae54b499f7318c
arocasan <mr@michelantonio.com>
2023-11-29 23:43:57 +0100

    Passing tests

--- tests/test_gh_actions_pass.py
+++ tests/test_gh_actions_pass.py
@@ -1,3 +1,8 @@
 class GitHubActionPassingTest:
     def test_github_action_passing(self):
         assert True
+
+
+class GitHubActionFailingTest:
+    def test_github_action_failing(self):
+        assert False
